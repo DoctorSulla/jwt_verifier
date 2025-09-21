9e6722639e70eac7eb4df41cac017d9d8cdfae35
Matthew Halliday <matthew.halliday@gmail.com>
2025-09-21 20:56:07 +0100

    Messing around

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,8 @@
-use base64::{DecodeError, Engine as _, engine::general_purpose::STANDARD_NO_PAD};
+use base64::{DecodeError, Engine as _, engine::general_purpose::URL_SAFE_NO_PAD};
+use rsa::pkcs1v15::Pkcs1v15Sign;
+use rsa::{BigUint, Pkcs1v15Encrypt, RsaPrivateKey, RsaPublicKey};
 use serde::{Deserialize, Serialize};
+use sha2::{Digest, Sha256};
 use thiserror::Error;
 
 #[derive(Error, Debug)]
@@ -39,6 +42,20 @@ struct GoogleJwt {
 //Metadata { alg: "RS256", kid: "07f078f2647e8cd019c40da9569e4f5247991094", typ: "JWT" }
 
 pub fn verify(raw_jwt: &str) -> Result<String, JwtParsingError> {
+    let raw_n = "pX0uFURVHarx3LZWaF4LnP3Kh2MbVl3iEOpQUcSxADEutXj383X9ZU6wdCmX4y_K23b0BU6oID1q0jkEE3sfQYaJJ7Qj9u2UnT-G9oGUoAn9GV1AYWxCNSz9mCrIJxP7ywcrvWJsKiYo7Q3Q-Tz44W1dCdVDQW870eixQSCnc6xrz4tu7RKrpeStH_GDhNIY3tXOuZvlPIvv4PH5sL39RaQ36T8ceGTWVDlYogKtvUUWl2YCGhz0f5y_ToRKU_WjnOmrN25_x30chCH3uz6I1RUa8vTAjbxCk4H5d1NmFNgV1zMSUKG0qo2d91fbyjmIRyODPVuUzSozREcVeSF_3Q";
+
+    let decoded_n = URL_SAFE_NO_PAD.decode(raw_n).unwrap();
+
+    let n = BigUint::from_bytes_be(&decoded_n);
+
+    let raw_e = "AQAB";
+
+    let decoded_e = URL_SAFE_NO_PAD.decode(raw_e.as_bytes()).unwrap();
+
+    let mut u32_bytes: Vec<u32> = decoded_e.iter().map(|v| *v as u32).collect();
+
+    let e = BigUint::new(u32_bytes);
+
     let parts: Vec<&str> = raw_jwt.split('.').collect();
     if parts.len() != 3 {
         return Err(JwtParsingError::InvalidNumberOfParts);
@@ -48,8 +65,19 @@ pub fn verify(raw_jwt: &str) -> Result<String, JwtParsingError> {
     let raw_payload = parts[1];
     let raw_signature = parts[2];
 
-    let decoded_metadata = STANDARD_NO_PAD.decode(raw_metadata.as_bytes()).unwrap();
-    let decoded_payload = STANDARD_NO_PAD.decode(raw_payload.as_bytes()).unwrap();
+    let to_sign = format!("{}.{}", raw_metadata, raw_payload);
+
+    let hash = Sha256::digest(to_sign);
+    println!("{:X?}", hash);
+
+    //println!("{:?}", hex);
+
+    let public_key = RsaPublicKey::new(n, e).unwrap();
+
+    //public_key.verify(to_sign, hashed, sig)
+
+    let decoded_metadata = URL_SAFE_NO_PAD.decode(raw_metadata.as_bytes()).unwrap();
+    let decoded_payload = URL_SAFE_NO_PAD.decode(raw_payload.as_bytes()).unwrap();
 
     let metadata: Metadata = serde_json::from_slice(&decoded_metadata).unwrap();
     let payload: GoogleJwt = serde_json::from_slice(&decoded_payload).unwrap();
