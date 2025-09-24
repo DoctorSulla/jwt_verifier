16cf3191d5ffe2150c72760b7c0628ae7a42f0a1
Matthew Halliday <matthew.halliday@gmail.com>
2025-09-24 21:36:39 +0100

    Added the ability to generate JWTs for testing

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,18 @@
 use base64::{DecodeError, Engine as _, engine::general_purpose::URL_SAFE_NO_PAD};
 use chrono::Utc;
 use reqwest::Client;
-use rsa::pkcs1v15::{Signature, VerifyingKey};
+use rsa::pkcs1v15::{Signature, SigningKey, VerifyingKey};
+use rsa::rand_core::impls::fill_bytes_via_next;
 use rsa::signature::Verifier;
 use rsa::traits::PublicKeyParts;
 use rsa::{BigUint, RsaPrivateKey, RsaPublicKey};
 use serde::{Deserialize, Serialize};
 use sha2::Sha256;
+use signature::{SignerMut,SignatureEncoding};
 use thiserror::Error;
 
 mod stubs;
+
 
 pub struct JwtVerifierClient {
     client: Client,
@@ -73,12 +76,13 @@ pub struct Claims {
     jti: String,
 }
 
-#[derive(Serialize, Deserialize, Debug)]
+
+#[derive(Serialize, Deserialize, Debug,Clone)]
 pub struct GoogleKeys {
     keys: Vec<Key>,
 }
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug,Clone)]
 pub struct Key {
     pub kid: String,
     pub alg: String,
@@ -136,9 +140,9 @@ impl JwtVerifierClient {
      * This method is only available in test environments and uses stubbed keys and a cache with a 10 year expiry.
      */
     #[cfg(test)]
-    pub async fn test_client() -> Result<Self, JwtParsingError> {
+    pub async fn test_client(keys: GoogleKeys) -> Result<Self, JwtParsingError> {
         let client = Client::new();
-        let cached_keys: GoogleKeys = serde_json::from_str(stubs::KEYS_STUB)?;
+        let cached_keys = keys;
         let cache_expiry = Utc::now().timestamp() + 3600 * 24 * 3650;
 
         Ok(Self {
@@ -264,12 +268,14 @@ pub fn generate_mock_keys() -> (GoogleKeys, RsaPrivateKey) {
         .expect("failed to generate a key");
     let pub_key = RsaPublicKey::from(&priv_key);
 
-    println!("{}", URL_SAFE_NO_PAD.encode(pub_key.n().to_string()));
+    let mut bytes = [0u8; 16];
+    fill_bytes_via_next(&mut rng, &mut bytes);
+    let kid = URL_SAFE_NO_PAD.encode(bytes);
 
     let key = Key {
         e: "AQAB".to_string(),
-        n: URL_SAFE_NO_PAD.encode(pub_key.n().to_string()),
-        kid: "AEFAEF".to_string(),
+        n: URL_SAFE_NO_PAD.encode(pub_key.n().to_bytes_be()),
+        kid,
         alg: "RS256".to_string(),
         kty: "RSA".to_string(),
         r#use: "sig".to_string(),
@@ -281,23 +287,75 @@ pub fn generate_mock_keys() -> (GoogleKeys, RsaPrivateKey) {
 }
 
 #[cfg(test)]
+pub fn generate_test_jwt(nbf: i64, iat:i64, exp: i64) -> (GoogleKeys,String) {
+    let (keys, private_key) = generate_mock_keys();
+    let meta = Metadata {
+        alg: "RS256".to_string(),
+        kid: keys.keys[0].kid.clone(),
+        typ: "JWT".to_string(),
+    };
+
+    let claims =  Claims { 
+    iss: "https://accounts.google.com".to_string(),
+    azp: Some("988343938519-vle7kps2l5f6cdnjluibda25o66h2jpn.apps.googleusercontent.com".to_string()), 
+    aud: "988343938519-vle7kps2l5f6cdnjluibda25o66h2jpn.apps.googleusercontent.com".to_string(), 
+    sub: "103095439063867298671".to_string(), 
+    hd: None, 
+    email: "matthew.halliday@gmail.com".to_string(), 
+    email_verified: true, 
+    nbf: nbf ,name: "Matthew Halliday".to_string(), 
+    picture: Some("https://lh3.googleusercontent.com/a/ACg8ocJBye9MzzYiLH5D4k9--Up5IYo3taQlbnwESHJgHz_hbAS8HVyx=s96-c".to_string()), 
+    given_name: "Matthew".to_string(), 
+    family_name: "Halliday".to_string(), 
+    iat: iat,
+    exp: exp,
+    jti: "ca7efff4133fc544efcba761f6839618fee9e8cf".to_string() };
+
+    let meta_encoded = URL_SAFE_NO_PAD.encode(serde_json::to_string(&meta).unwrap());
+    let claims_encoded = URL_SAFE_NO_PAD.encode(serde_json::to_string(&claims).unwrap());
+
+    let to_sign = format!("{meta_encoded}.{claims_encoded}");
+    let mut signing_key = SigningKey::<Sha256>::new(private_key);
+
+    let signature = signing_key.sign(to_sign.as_bytes()).to_bytes();
+    let encoded_signature = URL_SAFE_NO_PAD.encode(signature);
+    let jwt = format!("{meta_encoded}.{claims_encoded}.{encoded_signature}");
+(keys,jwt)
+}
+
+#[cfg(test)]
 mod tests {
     use super::*;
 
-    const VALID_JWT: &str = r##"eyJhbGciOiJSUzI1NiIsImtpZCI6IjA3ZjA3OGYyNjQ3ZThjZDAxOWM0MGRhOTU2OWU0ZjUyNDc5OTEwOTQiLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJodHRwczovL2FjY291bnRzLmdvb2dsZS5jb20iLCJhenAiOiI5ODgzNDM5Mzg1MTktdmxlN2twczJsNWY2Y2Ruamx1aWJkYTI1bzY2aDJqcG4uYXBwcy5nb29nbGV1c2VyY29udGVudC5jb20iLCJhdWQiOiI5ODgzNDM5Mzg1MTktdmxlN2twczJsNWY2Y2Ruamx1aWJkYTI1bzY2aDJqcG4uYXBwcy5nb29nbGV1c2VyY29udGVudC5jb20iLCJzdWIiOiIxMDMwOTU0MzkwNjM4NjcyOTg2NzEiLCJlbWFpbCI6Im1hdHRoZXcuaGFsbGlkYXlAZ21haWwuY29tIiwiZW1haWxfdmVyaWZpZWQiOnRydWUsIm5iZiI6MTc1ODQ0MDY1NiwibmFtZSI6Ik1hdHRoZXcgSGFsbGlkYXkiLCJwaWN0dXJlIjoiaHR0cHM6Ly9saDMuZ29vZ2xldXNlcmNvbnRlbnQuY29tL2EvQUNnOG9jSkJ5ZTlNenpZaUxINUQ0azktLVVwNUlZbzN0YVFsYm53RVNISmdIel9oYkFTOEhWeXg9czk2LWMiLCJnaXZlbl9uYW1lIjoiTWF0dGhldyIsImZhbWlseV9uYW1lIjoiSGFsbGlkYXkiLCJpYXQiOjE3NTg0NDA5NTYsImV4cCI6MTc1ODQ0NDU1NiwianRpIjoiY2E3ZWZmZjQxMzNmYzU0NGVmY2JhNzYxZjY4Mzk2MThmZWU5ZThjZiJ9.o5CVIXUiIZYW2-CBwiT9CjbaExnjiH90_QfX1r4hmKePachwLE_KG54p6octPwWx_L3COM4thQun7vx6k1ShFSx7x3pIit2BgwE6iJsZy9fSHdEGhjKnFuNZnocDyDCY94xYyaiLZCp5G39rPC8VmY8flBnnt5YjlmoX0pY_C_SzJhSZLe1oMSj1P4ZfyJkyg-sXtRMw32Z7whRCGN70_u9SkJXdZCoTNUWbkIQwzRrehW63Omw_9iRyRcZ9AbAlTxSi1YrkGJsrPclCK2HZmtXqybmlBgu2Zh6tejfDXGZntRrFMmx7QpJyuWRdPRmyPZSPXn8UOc3GrMuRUOF54g"##;
+    const _VALID_JWT: &str = r##"eyJhbGciOiJSUzI1NiIsImtpZCI6IjA3ZjA3OGYyNjQ3ZThjZDAxOWM0MGRhOTU2OWU0ZjUyNDc5OTEwOTQiLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJodHRwczovL2FjY291bnRzLmdvb2dsZS5jb20iLCJhenAiOiI5ODgzNDM5Mzg1MTktdmxlN2twczJsNWY2Y2Ruamx1aWJkYTI1bzY2aDJqcG4uYXBwcy5nb29nbGV1c2VyY29udGVudC5jb20iLCJhdWQiOiI5ODgzNDM5Mzg1MTktdmxlN2twczJsNWY2Y2Ruamx1aWJkYTI1bzY2aDJqcG4uYXBwcy5nb29nbGV1c2VyY29udGVudC5jb20iLCJzdWIiOiIxMDMwOTU0MzkwNjM4NjcyOTg2NzEiLCJlbWFpbCI6Im1hdHRoZXcuaGFsbGlkYXlAZ21haWwuY29tIiwiZW1haWxfdmVyaWZpZWQiOnRydWUsIm5iZiI6MTc1ODQ0MDY1NiwibmFtZSI6Ik1hdHRoZXcgSGFsbGlkYXkiLCJwaWN0dXJlIjoiaHR0cHM6Ly9saDMuZ29vZ2xldXNlcmNvbnRlbnQuY29tL2EvQUNnOG9jSkJ5ZTlNenpZaUxINUQ0azktLVVwNUlZbzN0YVFsYm53RVNISmdIel9oYkFTOEhWeXg9czk2LWMiLCJnaXZlbl9uYW1lIjoiTWF0dGhldyIsImZhbWlseV9uYW1lIjoiSGFsbGlkYXkiLCJpYXQiOjE3NTg0NDA5NTYsImV4cCI6MTc1ODQ0NDU1NiwianRpIjoiY2E3ZWZmZjQxMzNmYzU0NGVmY2JhNzYxZjY4Mzk2MThmZWU5ZThjZiJ9.o5CVIXUiIZYW2-CBwiT9CjbaExnjiH90_QfX1r4hmKePachwLE_KG54p6octPwWx_L3COM4thQun7vx6k1ShFSx7x3pIit2BgwE6iJsZy9fSHdEGhjKnFuNZnocDyDCY94xYyaiLZCp5G39rPC8VmY8flBnnt5YjlmoX0pY_C_SzJhSZLe1oMSj1P4ZfyJkyg-sXtRMw32Z7whRCGN70_u9SkJXdZCoTNUWbkIQwzRrehW63Omw_9iRyRcZ9AbAlTxSi1YrkGJsrPclCK2HZmtXqybmlBgu2Zh6tejfDXGZntRrFMmx7QpJyuWRdPRmyPZSPXn8UOc3GrMuRUOF54g"##;
 
     #[tokio::test]
     async fn valid_jwt_signature_matches() {
-        generate_mock_keys();
-        let mut jwt_client = JwtVerifierClient::test_client().await.unwrap();
+        let (keys,jwt) = generate_test_jwt(Utc::now().timestamp() -30, Utc::now().timestamp(), Utc::now().timestamp() + 3600);
+        let mut jwt_client = JwtVerifierClient::test_client(keys.clone()).await.unwrap();
         let result = jwt_client
             .verify(
-                VALID_JWT,
+                &jwt,
                 true,
                 "988343938519-vle7kps2l5f6cdnjluibda25o66h2jpn.apps.googleusercontent.com",
             )
             .await;
         println!("{:?}", result);
         assert!(result.is_ok());
+    }
+
+    #[tokio::test]
+    async fn expired_jwt_fails() {
+        let (keys,jwt) = generate_test_jwt(Utc::now().timestamp() -7230, Utc::now().timestamp() - 7200, Utc::now().timestamp() - 3600);
+        let mut jwt_client = JwtVerifierClient::test_client(keys.clone()).await.unwrap();
+        let result = jwt_client
+            .verify(
+                &jwt,
+                true,
+                "988343938519-vle7kps2l5f6cdnjluibda25o66h2jpn.apps.googleusercontent.com",
+            )
+            .await;
+        println!("{:?}", result);
+        assert!(result.is_err());
     }
 }
